9e2fa48a1f6659731de5ed715334683106925805
AnandhakumarG <anandhakumarsrg@outlook.com>
2023-12-27 17:05:53 +0530

    First COmmit

---
+++ src/test/java/test/test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class test {
+	
+
+	public void greeting() {
+		System.out.println(" Hello world");
+	}
+	
+	public static void main(String[] args){
+        test obj=new test();
+	obj.greeting();
+
+}
+}
