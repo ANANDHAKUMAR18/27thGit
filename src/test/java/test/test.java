58d9672d69c90d2e50bb42eaee83afddfbfde66d
Sandhiya0205 <146726602+Sandhiya0205@users.noreply.github.com>
2023-12-27 17:11:33 +0530

    Update test.java

--- src/test/java/test/test.java
+++ src/test/java/test/test.java
@@ -6,10 +6,14 @@ public class test {
 	public void greeting() {
 		System.out.println(" Hello world");
 	}
+	public void greetings() {
+		System.out.println(" Hello world");
+	}
 	
 	public static void main(String[] args){
         test obj=new test();
 	obj.greeting();
+	obj.greetings();
 
 }
 }
